1c12f6f43b9c1a8bb530ef37889c1c3e7aae92b4
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-10-10 13:30:03 -0500

    create custom api endpoint extensions to bypass email requirement + other future things

---
+++ server/Services/CustomIdentityApiEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,370 @@
+using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Http.Metadata;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.Extensions.Options;
+using server.Models;
+
+namespace server.Services;
+
+public static class CustomIdentityApiEndpointRouteBuilderExtensions {
+    /// <summary>
+    /// Add endpoints for registering, logging in, and logging out using ASP.NET Core Identity.
+    /// </summary>
+    /// <typeparam name="TUser">The type describing the user. This should match the generic parameter in <see cref="UserManager{TUser}"/>.</typeparam>
+    /// <param name="endpoints">
+    /// The <see cref="IEndpointRouteBuilder"/> to add the identity endpoints to.
+    /// Call <see cref="EndpointRouteBuilderExtensions.MapGroup(IEndpointRouteBuilder, string)"/> to add a prefix to all the endpoints.
+    /// </param>
+    /// <returns>An <see cref="IEndpointConventionBuilder"/> to further customize the added endpoints.</returns>
+    public static IEndpointConventionBuilder MapCustomIdentityApi<TUser>(this IEndpointRouteBuilder endpoints)
+        where TUser : class, new()
+    {
+        ArgumentNullException.ThrowIfNull(endpoints);
+
+        var timeProvider = endpoints.ServiceProvider.GetRequiredService<TimeProvider>();
+        var bearerTokenOptions = endpoints.ServiceProvider.GetRequiredService<IOptionsMonitor<BearerTokenOptions>>();
+        var linkGenerator = endpoints.ServiceProvider.GetRequiredService<LinkGenerator>();
+
+        var routeGroup = endpoints.MapGroup("");
+
+        // NOTE: We cannot inject UserManager<TUser> directly because the TUser generic parameter is currently unsupported by RDG.
+        // https://github.com/dotnet/aspnetcore/issues/47338
+        routeGroup.MapPost("/register", async Task<Results<Ok<RegisterResponse>, ValidationProblem>>
+            ([FromBody] CustomRegisterRequest registration, HttpContext context, [FromServices] IServiceProvider sp) =>
+        {
+            var userManager = sp.GetRequiredService<UserManager<TUser>>();
+
+            var userStore = sp.GetRequiredService<IUserStore<TUser>>();
+            var userName = registration.UserName;
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                return CreateValidationProblem(IdentityResult.Failed(userManager.ErrorDescriber.InvalidUserName(userName)));
+            }
+
+            var user = new TUser();
+            await userStore.SetUserNameAsync(user, userName, CancellationToken.None);
+            var result = await userManager.CreateAsync(user, registration.Password);
+
+            if (!result.Succeeded)
+            {
+                return CreateValidationProblem(result);
+            }
+
+            var response = new RegisterResponse {
+                Message = "user registration successful"
+            };
+
+            return TypedResults.Ok(response);
+        });
+
+        routeGroup.MapPost("/login", async Task<Results<Ok<AccessTokenResponse>, EmptyHttpResult, ProblemHttpResult>>
+            ([FromBody] CustomLoginRequest login, [FromQuery] bool? useCookies, [FromQuery] bool? useSessionCookies, [FromServices] IServiceProvider sp) =>
+        {
+            var signInManager = sp.GetRequiredService<SignInManager<TUser>>();
+
+            var useCookieScheme = (useCookies == true) || (useSessionCookies == true);
+            var isPersistent = (useCookies == true) && (useSessionCookies != true);
+            signInManager.AuthenticationScheme = useCookieScheme ? IdentityConstants.ApplicationScheme : IdentityConstants.BearerScheme;
+
+            var result = await signInManager.PasswordSignInAsync(login.UserName, login.Password, isPersistent, lockoutOnFailure: true);
+
+            if (result.RequiresTwoFactor)
+            {
+                if (!string.IsNullOrEmpty(login.TwoFactorCode))
+                {
+                    result = await signInManager.TwoFactorAuthenticatorSignInAsync(login.TwoFactorCode, isPersistent, rememberClient: isPersistent);
+                }
+                else if (!string.IsNullOrEmpty(login.TwoFactorRecoveryCode))
+                {
+                    result = await signInManager.TwoFactorRecoveryCodeSignInAsync(login.TwoFactorRecoveryCode);
+                }
+            }
+
+            if (!result.Succeeded)
+            {
+                return TypedResults.Problem(result.ToString(), statusCode: StatusCodes.Status401Unauthorized);
+            }
+
+            // The signInManager already produced the needed response in the form of a cookie or bearer token.
+            return TypedResults.Empty;
+        });
+
+        routeGroup.MapPost("/refresh", async Task<Results<Ok<AccessTokenResponse>, UnauthorizedHttpResult, SignInHttpResult, ChallengeHttpResult>>
+            ([FromBody] RefreshRequest refreshRequest, [FromServices] IServiceProvider sp) =>
+        {
+            var signInManager = sp.GetRequiredService<SignInManager<TUser>>();
+            var refreshTokenProtector = bearerTokenOptions.Get(IdentityConstants.BearerScheme).RefreshTokenProtector;
+            var refreshTicket = refreshTokenProtector.Unprotect(refreshRequest.RefreshToken);
+
+            // Reject the /refresh attempt with a 401 if the token expired or the security stamp validation fails
+            if (refreshTicket?.Properties?.ExpiresUtc is not { } expiresUtc ||
+                timeProvider.GetUtcNow() >= expiresUtc ||
+                await signInManager.ValidateSecurityStampAsync(refreshTicket.Principal) is not TUser user)
+
+            {
+                return TypedResults.Challenge();
+            }
+
+            var newPrincipal = await signInManager.CreateUserPrincipalAsync(user);
+            return TypedResults.SignIn(newPrincipal, authenticationScheme: IdentityConstants.BearerScheme);
+        });
+
+        // routeGroup.MapPost("/forgotPassword", async Task<Results<Ok, ValidationProblem>>
+        //     ([FromBody] ForgotPasswordRequest resetRequest, [FromServices] IServiceProvider sp) =>
+        // {
+        //     var userManager = sp.GetRequiredService<UserManager<TUser>>();
+        //     var user = await userManager.FindByEmailAsync(resetRequest.Email);
+
+        //     if (user is not null && await userManager.IsEmailConfirmedAsync(user))
+        //     {
+        //         var code = await userManager.GeneratePasswordResetTokenAsync(user);
+        //         code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+        //         await emailSender.SendPasswordResetCodeAsync(user, resetRequest.Email, HtmlEncoder.Default.Encode(code));
+        //     }
+
+        //     // Don't reveal that the user does not exist or is not confirmed, so don't return a 200 if we would have
+        //     // returned a 400 for an invalid code given a valid user email.
+        //     return TypedResults.Ok();
+        // });
+
+        // routeGroup.MapPost("/resetPassword", async Task<Results<Ok, ValidationProblem>>
+        //     ([FromBody] ResetPasswordRequest resetRequest, [FromServices] IServiceProvider sp) =>
+        // {
+        //     var userManager = sp.GetRequiredService<UserManager<TUser>>();
+
+        //     var user = await userManager.FindByEmailAsync(resetRequest.Email);
+
+        //     if (user is null || !(await userManager.IsEmailConfirmedAsync(user)))
+        //     {
+        //         // Don't reveal that the user does not exist or is not confirmed, so don't return a 200 if we would have
+        //         // returned a 400 for an invalid code given a valid user email.
+        //         return CreateValidationProblem(IdentityResult.Failed(userManager.ErrorDescriber.InvalidToken()));
+        //     }
+
+        //     IdentityResult result;
+        //     try
+        //     {
+        //         var code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(resetRequest.ResetCode));
+        //         result = await userManager.ResetPasswordAsync(user, code, resetRequest.NewPassword);
+        //     }
+        //     catch (FormatException)
+        //     {
+        //         result = IdentityResult.Failed(userManager.ErrorDescriber.InvalidToken());
+        //     }
+
+        //     if (!result.Succeeded)
+        //     {
+        //         return CreateValidationProblem(result);
+        //     }
+
+        //     return TypedResults.Ok();
+        // });
+
+        var accountGroup = routeGroup.MapGroup("/manage").RequireAuthorization();
+
+        accountGroup.MapPost("/2fa", async Task<Results<Ok<TwoFactorResponse>, ValidationProblem, NotFound>>
+            (ClaimsPrincipal claimsPrincipal, [FromBody] TwoFactorRequest tfaRequest, [FromServices] IServiceProvider sp) =>
+        {
+            var signInManager = sp.GetRequiredService<SignInManager<TUser>>();
+            var userManager = signInManager.UserManager;
+            if (await userManager.GetUserAsync(claimsPrincipal) is not { } user)
+            {
+                return TypedResults.NotFound();
+            }
+
+            if (tfaRequest.Enable == true)
+            {
+                if (tfaRequest.ResetSharedKey)
+                {
+                    return CreateValidationProblem("CannotResetSharedKeyAndEnable",
+                        "Resetting the 2fa shared key must disable 2fa until a 2fa token based on the new shared key is validated.");
+                }
+                else if (string.IsNullOrEmpty(tfaRequest.TwoFactorCode))
+                {
+                    return CreateValidationProblem("RequiresTwoFactor",
+                        "No 2fa token was provided by the request. A valid 2fa token is required to enable 2fa.");
+                }
+                else if (!await userManager.VerifyTwoFactorTokenAsync(user, userManager.Options.Tokens.AuthenticatorTokenProvider, tfaRequest.TwoFactorCode))
+                {
+                    return CreateValidationProblem("InvalidTwoFactorCode",
+                        "The 2fa token provided by the request was invalid. A valid 2fa token is required to enable 2fa.");
+                }
+
+                await userManager.SetTwoFactorEnabledAsync(user, true);
+            }
+            else if (tfaRequest.Enable == false || tfaRequest.ResetSharedKey)
+            {
+                await userManager.SetTwoFactorEnabledAsync(user, false);
+            }
+
+            if (tfaRequest.ResetSharedKey)
+            {
+                await userManager.ResetAuthenticatorKeyAsync(user);
+            }
+
+            string[]? recoveryCodes = null;
+            if (tfaRequest.ResetRecoveryCodes || (tfaRequest.Enable == true && await userManager.CountRecoveryCodesAsync(user) == 0))
+            {
+                var recoveryCodesEnumerable = await userManager.GenerateNewTwoFactorRecoveryCodesAsync(user, 10);
+                recoveryCodes = recoveryCodesEnumerable?.ToArray();
+            }
+
+            if (tfaRequest.ForgetMachine)
+            {
+                await signInManager.ForgetTwoFactorClientAsync();
+            }
+
+            var key = await userManager.GetAuthenticatorKeyAsync(user);
+            if (string.IsNullOrEmpty(key))
+            {
+                await userManager.ResetAuthenticatorKeyAsync(user);
+                key = await userManager.GetAuthenticatorKeyAsync(user);
+
+                if (string.IsNullOrEmpty(key))
+                {
+                    throw new NotSupportedException("The user manager must produce an authenticator key after reset.");
+                }
+            }
+
+            return TypedResults.Ok(new TwoFactorResponse
+            {
+                SharedKey = key,
+                RecoveryCodes = recoveryCodes,
+                RecoveryCodesLeft = recoveryCodes?.Length ?? await userManager.CountRecoveryCodesAsync(user),
+                IsTwoFactorEnabled = await userManager.GetTwoFactorEnabledAsync(user),
+                IsMachineRemembered = await signInManager.IsTwoFactorClientRememberedAsync(user),
+            });
+        });
+
+        accountGroup.MapGet("/info", async Task<Results<Ok<InfoResponse>, ValidationProblem, NotFound>>
+            (ClaimsPrincipal claimsPrincipal, [FromServices] IServiceProvider sp) =>
+        {
+            var userManager = sp.GetRequiredService<UserManager<TUser>>();
+            if (await userManager.GetUserAsync(claimsPrincipal) is not { } user)
+            {
+                return TypedResults.NotFound();
+            }
+
+            return TypedResults.Ok(await CreateInfoResponseAsync(user, userManager));
+        });
+
+        accountGroup.MapPost("/info", async Task<Results<Ok<InfoResponse>, ValidationProblem, NotFound>>
+            (ClaimsPrincipal claimsPrincipal, [FromBody] InfoRequest infoRequest, HttpContext context, [FromServices] IServiceProvider sp) =>
+        {
+            var userManager = sp.GetRequiredService<UserManager<TUser>>();
+            if (await userManager.GetUserAsync(claimsPrincipal) is not { } user)
+            {
+                return TypedResults.NotFound();
+            }
+
+            // if (!string.IsNullOrEmpty(infoRequest.NewEmail) && !_emailAddressAttribute.IsValid(infoRequest.NewEmail))
+            // {
+            //     return CreateValidationProblem(IdentityResult.Failed(userManager.ErrorDescriber.InvalidEmail(infoRequest.NewEmail)));
+            // }
+
+            if (!string.IsNullOrEmpty(infoRequest.NewPassword))
+            {
+                if (string.IsNullOrEmpty(infoRequest.OldPassword))
+                {
+                    return CreateValidationProblem("OldPasswordRequired",
+                        "The old password is required to set a new password. If the old password is forgotten, use /resetPassword.");
+                }
+
+                var changePasswordResult = await userManager.ChangePasswordAsync(user, infoRequest.OldPassword, infoRequest.NewPassword);
+                if (!changePasswordResult.Succeeded)
+                {
+                    return CreateValidationProblem(changePasswordResult);
+                }
+            }
+
+            // if (!string.IsNullOrEmpty(infoRequest.NewEmail))
+            // {
+            //     var email = await userManager.GetEmailAsync(user);
+
+            //     if (email != infoRequest.NewEmail)
+            //     {
+            //         await SendConfirmationEmailAsync(user, userManager, context, infoRequest.NewEmail, isChange: true);
+            //     }
+            // }
+
+            return TypedResults.Ok(await CreateInfoResponseAsync(user, userManager));
+        });
+
+        return new IdentityEndpointsConventionBuilder(routeGroup);
+    }
+
+    private static ValidationProblem CreateValidationProblem(string errorCode, string errorDescription) =>
+        TypedResults.ValidationProblem(new Dictionary<string, string[]> {
+            { errorCode, [errorDescription] }
+        });
+
+    private static ValidationProblem CreateValidationProblem(IdentityResult result)
+    {
+        // We expect a single error code and description in the normal case.
+        // This could be golfed with GroupBy and ToDictionary, but perf! :P
+        Debug.Assert(!result.Succeeded);
+        var errorDictionary = new Dictionary<string, string[]>(1);
+
+        foreach (var error in result.Errors)
+        {
+            string[] newDescriptions;
+
+            if (errorDictionary.TryGetValue(error.Code, out var descriptions))
+            {
+                newDescriptions = new string[descriptions.Length + 1];
+                Array.Copy(descriptions, newDescriptions, descriptions.Length);
+                newDescriptions[descriptions.Length] = error.Description;
+            }
+            else
+            {
+                newDescriptions = [error.Description];
+            }
+
+            errorDictionary[error.Code] = newDescriptions;
+        }
+
+        return TypedResults.ValidationProblem(errorDictionary);
+    }
+
+    private static async Task<InfoResponse> CreateInfoResponseAsync<TUser>(TUser user, UserManager<TUser> userManager)
+        where TUser : class
+    {
+        return new()
+        {
+            Email = await userManager.GetEmailAsync(user) ?? throw new NotSupportedException("Users must have an email."),
+            IsEmailConfirmed = await userManager.IsEmailConfirmedAsync(user),
+        };
+    }
+
+    // Wrap RouteGroupBuilder with a non-public type to avoid a potential future behavioral breaking change.
+    private sealed class IdentityEndpointsConventionBuilder(RouteGroupBuilder inner) : IEndpointConventionBuilder
+    {
+        private IEndpointConventionBuilder InnerAsConventionBuilder => inner;
+
+        public void Add(Action<EndpointBuilder> convention) => InnerAsConventionBuilder.Add(convention);
+        public void Finally(Action<EndpointBuilder> finallyConvention) => InnerAsConventionBuilder.Finally(finallyConvention);
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter)]
+    private sealed class FromBodyAttribute : Attribute, IFromBodyMetadata
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter)]
+    private sealed class FromServicesAttribute : Attribute, IFromServiceMetadata
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter)]
+    private sealed class FromQueryAttribute : Attribute, IFromQueryMetadata
+    {
+        public string? Name => null;
+    }
+
+}
