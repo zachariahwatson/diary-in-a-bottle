67c5e9574355c0f680070c860f14cf990b361519
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-10-11 17:48:51 -0500

    install components

--- client/components/ui/index.ts
+++ client/components/ui/index.ts
@@ -4,3 +4,35 @@ export { Input } from "./input"
 export { Label } from "./label"
 export { Separator } from "./separator"
 export { Toaster } from "./sonner"
+export {
+	Select,
+	SelectGroup,
+	SelectValue,
+	SelectTrigger,
+	SelectContent,
+	SelectLabel,
+	SelectItem,
+	SelectSeparator,
+	SelectScrollUpButton,
+	SelectScrollDownButton,
+} from "./select"
+export {
+	DropdownMenu,
+	DropdownMenuTrigger,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuCheckboxItem,
+	DropdownMenuRadioItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuShortcut,
+	DropdownMenuGroup,
+	DropdownMenuPortal,
+	DropdownMenuSub,
+	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+	DropdownMenuRadioGroup,
+} from "./dropdown-menu"
+export { ThemeProvider } from "./theme-provider"
+export { Switch } from "./switch"
+export { RadioGroup, RadioGroupItem } from "./radio-group"
