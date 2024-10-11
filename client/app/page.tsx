a2deadb4eb27e9e06f17c2ba08a050ee34885651
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-10-11 17:49:23 -0500

    replace api call with hook

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,31 +1,13 @@
 "use client"
 
-import { useQuery } from "@tanstack/react-query"
+import { useUser } from "@/hooks/state"
 
 export default function Home() {
-	const { data, isLoading } = useQuery({
-		queryKey: ["weather"],
-		queryFn: async () => {
-			const url = new URL(`${process.env.NEXT_PUBLIC_URL}/api/weatherforecast`)
-			const response = await fetch(url, {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			})
-
-			if (!response.ok) {
-				const body = await response.json()
-				throw new Error(body.error)
-			}
-
-			return await response.json()
-		},
-	})
+	const { data: user, isLoading } = useUser()
 
 	return (
 		<div className="max-w-sm md:max-w-4xl w-full relative">
-			{isLoading ? "loading..." : JSON.stringify(data, null, 2)}
+			{isLoading ? "user loading..." : JSON.stringify(user, null, 2)}
 		</div>
 	)
 }
