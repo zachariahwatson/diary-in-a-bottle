857a91ee73a2f27baf0f57b72459fea9b209e3ac
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-10-12 23:14:26 -0500

    add textarea

---
+++ client/components/ui/textarea.tsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+	initialCharacterCount?: number
+}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+	({ className, initialCharacterCount = 0, ...props }, ref) => {
+		const [characterCount, setCharacterCount] = React.useState<number>(initialCharacterCount)
+
+		const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+			setCharacterCount(event.target.value.length)
+			if (props.onChange) {
+				props.onChange(event) // Call the original onChange handler if it exists
+			}
+		}
+
+		return (
+			<div className="relative h-full">
+				<textarea
+					className={cn(
+						"flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+						className
+					)}
+					ref={ref}
+					{...props}
+					onChange={handleChange}
+				/>
+				{characterCount > 0 && (
+					<div className="text-xs text-muted-foreground absolute right-0 bottom-full mb-2">{characterCount}</div>
+				)}
+			</div>
+		)
+	}
+)
+Textarea.displayName = "Textarea"
+
+export { Textarea }
