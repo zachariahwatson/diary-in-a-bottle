a3e04403e48381aff3528aa889c8c2a46683fec4
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-10-12 21:12:58 -0500

    added min character count

--- client/lib/zod/signin-form-schema.ts
+++ client/lib/zod/signin-form-schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
 
 export const signInFormSchema = z.object({
-	userName: z.string({ required_error: "username is requred" }),
-	password: z.string({ required_error: "password is requred" }),
+	userName: z.string({ required_error: "username is requred" }).min(1, "username must not be empty"),
+	password: z.string({ required_error: "password is requred" }).min(1, "password must not be empty"),
 })
