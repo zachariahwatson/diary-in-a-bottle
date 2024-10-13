e946f943ac27dcabe167c294df8f85f284a2947c
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-10-12 23:09:36 -0500

    remove styling

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,9 +5,5 @@ import { useUser } from "@/hooks/state"
 export default function Home() {
 	const { data: user, isLoading } = useUser()
 
-	return (
-		<div className="max-w-sm md:max-w-4xl w-full relative">
-			{isLoading ? "user loading..." : JSON.stringify(user, null, 2)}
-		</div>
-	)
+	return <div>{isLoading ? "user loading..." : JSON.stringify(user, null, 2)}</div>
 }
